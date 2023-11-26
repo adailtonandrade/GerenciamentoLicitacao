9856361b2a8e3efdacadc3ae22e1b42e05c25d26
Adailton Andrade <adailtonsts@gmail.com>
2023-11-26 08:49:47 -0300

    Adicionado Entitidade de Licitacao(Bidding), Adicionado o Configuration para Entidade, Ajuste no EntityBase

---
+++ Data/Mappings/BiddingConfiguration .cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Enum;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Mappings
+{
+    internal class BiddingConfiguration : EntityTypeConfiguration<Bidding>
+    {
+        protected override void Configure(EntityTypeBuilder<Bidding> builder)
+        {
+            builder.Property(e => e.Status)
+                .HasConversion(v => v.ToString(), v => (BiddingStatusEnum)Enum.Parse(typeof(BiddingStatusEnum), v));
+
+            builder.Property(p => p.Description)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            builder.Property(p => p.Number)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+        }
+    }
+}
---
+++ Domain/Enum/BiddingStatusEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enum
+{
+    public enum BiddingStatusEnum
+    {
+        Opened,
+        InProgress,
+        Closed
+    }
+}
